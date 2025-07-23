de10a3e82930e0519e5eeb85f893507cedcc3e70
thl <1306226780@qq.com>
2025-07-23 10:50:14 +0800

    新增员工业务代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,31 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "添加员工时传递的数据模型")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("员工id，默认不传")
     private Long id;
 
+    @ApiModelProperty("员工用户名")
     private String username;
 
+    @ApiModelProperty("员工真实性名")
     private String name;
 
+    @ApiModelProperty("员工电话号码")
     private String phone;
 
+    @ApiModelProperty("员工性别")
     private String sex;
 
+    @ApiModelProperty("员工身份账号")
     private String idNumber;
 
 }
